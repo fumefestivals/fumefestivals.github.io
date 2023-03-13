f5066cc2644648120ef62d31fce3a80e1a2fc0fb
sotschiwebsite <contact@sotschiofficial.com>
2023-03-13 01:38:14 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-/**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
- */
-
 (function ($) {
   'use strict';
 
@@ -93,8 +86,8 @@
   // clients logo slider
   $('.client-logo-slider').slick({
     infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     autoplay: true,
     dots: false,
     arrows: false,
