e38e95965afb8c44fd8cce7f2c291e12ae8b22ca
Sotschi <contact@sotschiofficial.com>
2023-10-03 19:08:26 +0200

    sadd

--- js/fileupload.js
+++ js/fileupload.js
@@ -34,3 +34,80 @@ function _(el){
     setInterval(function () { window.location.href = "../success.html";}, 3000);
     myButton.disabled = true; return true;
 }
+document.addEventListener("DOMContentLoaded", function() {
+  const myForm = document.getElementById("PromoterApplicationForm");
+  const field1 = document.getElementById("FirstName");
+  const field2 = document.getElementById("LastName");
+  const field3 = document.getElementById("PhoneNumber");
+  const field4 = document.getElementById("Email");
+  const field5 = document.getElementById("Followers");
+  const field6 = document.getElementById("Location");
+  const field7 = document.getElementById("Instagram");
+  const field8 = document.getElementById("promotertype");
+  const field9 = document.getElementById("file1");
+  const submitButton = document.getElementById("submitButton");
+
+  // Function to check if all fields are filled
+  function areFieldsFilled() {
+      return field1.value.trim() !== "" && 
+      field2.value.trim() !== "" && 
+      field3.value.trim() !== "" && 
+      field4.value.trim() !== "" && 
+      field5.value.trim() !== "" && 
+      field6.value.trim() !== "" && 
+      field7.value.trim() !== "" && 
+      field8.value.trim() !== "" && 
+      field9.value.trim() !== "";
+  }
+
+  // Function to toggle the submit button's disabled state and text
+  function toggleSubmitButton() {
+      const isFilled = areFieldsFilled();
+      submitButton.disabled = !isFilled;
+      submitButton.textContent = isFilled ? "Apply" : "Please fill out the required information";
+  }
+
+  // Add event listeners to form fields to check and toggle the button state
+  field1.addEventListener("input", toggleSubmitButton);
+  field2.addEventListener("input", toggleSubmitButton);
+  field3.addEventListener("input", toggleSubmitButton);
+  field4.addEventListener("input", toggleSubmitButton);
+  field5.addEventListener("input", toggleSubmitButton);
+  field6.addEventListener("input", toggleSubmitButton);
+  field7.addEventListener("input", toggleSubmitButton);
+  field8.addEventListener("input", toggleSubmitButton);
+  field9.addEventListener("input", toggleSubmitButton);
+
+  // Initially, check and set the submit button state
+  toggleSubmitButton();
+
+  // Handle form submission
+  PromoterApplicationForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the form from submitting (you can remove this if needed)
+
+      // Change the submit button text to "Loading..."
+      submitButton.textContent = "Please Wait";
+
+      // Simulate a delay (you can replace this with your actual form submission logic)
+      setTimeout(function() {
+        // Your form submission logic here
+        // This code will only execute when all fields are filled out.
+        alert("Form submitted!");
+
+        // Clear the form or perform any other post-submission actions here
+
+        // Re-enable the input fields after submission
+        field1.value = "";
+        field2.value = "";
+        field3.value = "";
+        field4.value = "";
+        field5.value = "";
+        field6.value = "";
+        field7.value = "";
+        field8.value = "";
+        field9.value = "";
+        
+        toggleSubmitButton();
+      }, 3000); // Simulated 2-second delay; replace with your actual submission logic
+  });
+});
