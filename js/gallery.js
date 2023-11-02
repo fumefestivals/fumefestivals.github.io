59323eff4faebc5a8593784cfbc640624693eba7
Sotschi <contact@sotschiofficial.com>
2023-11-02 22:25:26 +0100

    ss

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,36 @@
-<script>
-// Lazy load images using Intersection Observer
-const gallery = document.getElementById("lazy-load-gallery");
-const images = gallery.querySelectorAll("img[data-src]");
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.getElementById('gallery');
+  const anchors = gallery.getElementsByTagName('a');
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const img = entry.target;
-            img.src = img.getAttribute("data-src");
-            img.removeAttribute("data-src");
-            observer.unobserve(img);
-        }
+  for (const anchor of anchors) {
+      anchor.addEventListener('mouseenter', function () {
+          const image = new Image();
+          image.src = anchor.getAttribute('data-src');
+          image.alt = anchor.textContent;
+          anchor.appendChild(image);
+          anchor.style.display = 'block';
+      });
+  }
+});
+function lazyLoadImages(container) {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        img.src = img.getAttribute('data-src');
+        observer.unobserve(img);
+      }
     });
-});
+  });
 
-images.forEach(img => {
-    observer.observe(img);
-});
-</script>
+  const images = container.querySelectorAll('img[data-src]');
+  images.forEach(img => {
+    io.observe(img);
+  });
+}
+
+// Select the container with the class "image-gallery" and ID "gallery"
+const imageGallery = document.getElementById('gallery');
+
+// Lazy load all images inside the container
+lazyLoadImages(imageGallery);
